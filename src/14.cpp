613e4b407d590249634dee6432e294346b0e954d
mirandgiubbini310 <mirandgiubbini310@gmail.com>
2025-03-19 10:42:42 +0000

    Add 14.cpp

---
+++ src/14.cpp
@@ -0,0 +1,11 @@
+int main() {
+    int x = 5;
+    if (x == 0) {
+        cout << "x is equal to 0" << endl;
+    } else if (x < 0) {
+        cout << "x is negative" << endl;
+    } else {
+        cout << "x is positive or zero" << endl;
+    }
+    return 0;
+}
