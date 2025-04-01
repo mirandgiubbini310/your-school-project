617065ec4bfcc8c999a5585668e4f36c6777cdbf
mirandgiubbini310 <mirandgiubbini310@gmail.com>
2025-04-01 15:11:21 +0000

    Add 21.cpp

---
+++ src/21.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 10;
+    float c = 7.5f;
+    
+    cout << "The sum of a and b is: " << (a + b) << endl;
+    cout << "The product of a and b is: " << (a * b) << endl;
+    cout << "The square root of a is: " << sqrt(a) << endl;
+    cout << "The cube root of b is: " << cube_root(b) << endl;
+    
+    return 0;
+}
