d5c50fa4d3dda71ea2755fdf11ab6409f4435758
mirandgiubbini310 <mirandgiubbini310@gmail.com>
2025-04-07 03:05:51 +0000

    Add 25.cpp

---
+++ src/25.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 3;
+
+    if (x > y) {
+        cout << "x is greater than y." << endl;
+    } else {
+        cout << "x is not greater than y." << endl;
+    }
+
+    return 0;
+}
